88a1c812d3d9de82f6d0f6bd6963dd58352afb77
Linck7 <hattingerse@gmail.com>
2015-02-22 23:08:19 +0100

    indexxx
    indexxxxxx

---
+++ info.php
@@ -0,0 +1,17 @@
+<?php 
+session_start();
+include("inc/dbcon.php");
+
+include("inc/header.php");
+?>
+
+
+<div class="container">
+<div class="jumbotron">
+  <h1>Metactics</h1>      
+  <p>Info</p>      
+</div>
+
+<?php
+include("inc/footer.php");
+?>
